5e8f71b56f64b7c4c8079c5f3ecaa772a0f328e5
aryan02420 <aryan02420@gmail.com>
2021-04-04 22:40:27 +0530

    updated quicksort

--- src/algos/quick.js
+++ src/algos/quick.js
@@ -1,24 +1,25 @@
+const partition = function*(arr, lo, hi) {
+  let pivot = arr[hi];
+  let i = lo;
+  for (let j = lo; j < hi; j++) {
+    if (arr[j] < pivot) {
+      yield {array: arr.slice(), i: i, j: j, k: [hi]};
+      arr.swap(i, j);
+      yield {array: arr.slice(), i: i, j: j, k: [hi]};
+      i++;
+    }
+  }
+  yield {array: arr.slice(), i: i, j: hi, k: [hi]};
+  arr.swap(i, hi);
+  yield {array: arr.slice(), i: i, j: hi, k: [hi]};
+  return i;
+}
+
 const quicksort = function*(arr, lo, hi) {
   lo = lo ?? 0;
   hi = hi ?? arr.length;
   if (lo < hi) {
-      let p;
-
-          let pivot = arr[hi];
-          let i = lo;
-          for (let j = lo; j < hi; j++) {
-            if (arr[j] < pivot) {
-              yield {array: arr.slice(), i: i, j: j, k: [hi]};
-              arr.swap(i, j);
-              yield {array: arr.slice(), i: i, j: j, k: [hi]};
-              i++;
-            }
-          }
-          yield {array: arr.slice(), i: i, j: hi, k: [hi]};
-          arr.swap(i, hi);
-          yield {array: arr.slice(), i: i, j: hi, k: [hi]};
-          p = i;
-
+      let p = yield* partition(arr, lo, hi)
       yield* quicksort(arr, lo, p-1);
       yield* quicksort(arr, p+1, hi);
       }
